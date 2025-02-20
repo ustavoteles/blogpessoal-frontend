23be5645e1ff53ab1200cb9d4a23d18fce0491d2
ustavoteles <telesgustavo.dev@gmail.com>
2025-02-20 14:48:00 -0300

    Adicionando o modal postagem, editando e deletando postagem

---
+++ src/components/postagens/modalpostagem/ModalPostagem.tsx
@@ -0,0 +1,24 @@
+import Popup from 'reactjs-popup';
+import FormPostagem from '../formpostagem/FormPostagem';
+
+import 'reactjs-popup/dist/index.css';
+import './ModalPostagem.css'
+
+function ModalPostagem() {
+    return (
+        <>
+            <Popup
+                trigger={
+                    <button className="hover:bg-white hover:text-black cursor-pointer rounded text-white border-white border-2 py-2 px-4 shadow-xl shadow-blue-600/50 ">
+                        Nova Postagem
+                    </button>
+                }
+                modal
+            >
+                <FormPostagem />
+            </Popup>
+        </>
+    );
+}
+
+export default ModalPostagem;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,32 +1,31 @@
 import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem";
 
 function Home() {
   return (
     <>
-    <div className="bg-blue-900 flex justify-center">
-      <div className="container grid grid-cols-2 text-white">
-        <div className="flex flex-col gap-4 items-center justify-center py-4">
-          <h2 className="text-5xl font-bold">Seja Bem-Vindo(a)!</h2>
-          <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
-          <div className="flex justify-around gap-4">
-            <button className="rounded text-white border-white border-2 py-2 px-4 shadow-xl shadow-blue-600/50 ">
-              Nova Postagem
-            </button>
+      <div className="bg-blue-900 flex justify-center">
+        <div className="container grid grid-cols-2 text-white">
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-5xl font-bold">Seja Bem-Vindo(a)!</h2>
+            <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
+            <div className="flex justify-around gap-4">
+                <ModalPostagem /> 
+            </div>
+          </div>
+
+          <div className="flex justify-center">
+            <img
+              src="https://i.imgur.com/VpwApCU.png"
+              alt="Imagem da Página Home"
+              className="w-2/3"
+            />
           </div>
         </div>
-
-        <div className="flex justify-center">
-          <img
-            src="https://i.imgur.com/VpwApCU.png"
-            alt="Imagem da Página Home"
-            className="w-2/3"
-          />
-        </div>
       </div>
-    </div>
-<ListaPostagens />
-</>
-);
+      <ListaPostagens />
+    </>
+  );
 }
 
 export default Home;
